30f43932413c858efbc0548c60babcdd87c4e2c7
kunalrbhatia <kunal.bhatia@dailybooks.in>
2020-11-10 12:44:03 +0530

    commit

--- src/components/loggedin/logged.js
+++ src/components/loggedin/logged.js
@@ -1,28 +1,36 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
 export class LoggedIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { msgFromServer: "" };
+  }
   componentDidMount() {
     this.token = localStorage.getItem("token");
     if (this.token && this.token.length > 1) {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("auth-token", this.token + "");
       const requestOptions = {
         method: "POST",
-        headers: { "auth-token": this.token + "" },
+        headers: myHeaders,
       };
       fetch("http://localhost:5000/post/", requestOptions)
         .then((response) => response.json())
         .then((data) => {
-          this.setState({ uid: data._id });
-          //this.props.history.push("in");
+          this.setState({ uid: data.uid, msgFromServer: data.data });
         });
     } else {
       this.props.history.push("/");
     }
   }
   render() {
+    const { msgFromServer } = this.state;
     return (
       <div style={{ margin: "auto", width: " 50%", padding: "10px" }}>
         <Paper elevation={3} style={{ padding: 40, margin: 20 }}>
           <h3>Welcome</h3>
+          <h5>{msgFromServer}</h5>
         </Paper>
       </div>
     );
